3fc5c7e095c5b73d1e6468982fa5b8cf60d8ca8d
lokesh <kokullalokeh@gmail.com>
2025-04-13 17:28:01 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ class DEPT(models.Model):
     dname=models.CharField(max_length=100)
     dloc=models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.dname
+
 class EMP(models.Model):
     empno=models.IntegerField(primary_key=True)
     ename=models.CharField(max_length=100)
@@ -17,7 +20,12 @@ class EMP(models.Model):
     dept_no=models.ForeignKey(DEPT,on_delete=models.CASCADE)
     MGR=models.ForeignKey('self',on_delete=models.SET_NULL,null=True,blank=True)
 
+    def __str__(self):
+        return self.ename
+
+
 class SALGRADE(models.Model):
     high_sal=models.DecimalField(max_digits=10,decimal_places=3)
     low_sal=models.DecimalField(max_digits=10,decimal_places=3)
+    
 
--- app/views.py
+++ app/views.py
@@ -1,3 +1,108 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from app.models import *
 
-# Create your views here.
+
+
+def EmpToDeptJoin(request):
+    QLEDO=EMP.objects.all().select_related('dept_no')
+    d={'QLEDO':QLEDO}
+    return render(request,'EmpToDeptJoin.html',d)
+
+def EmpToMgrJoin(request):
+    QLEMO=EMP.objects.all().select_related('MGR')
+
+
+
+
+    QLEMO=EMP.objects.all().select_related('MGR').filter(sal__gt=5000)
+    QLEMO=EMP.objects.all().select_related('MGR').filter(ename__startswith='v')
+    QLEMO=EMP.objects.all().select_related('MGR').filter(ename__endswith='d')
+    QLEMO=EMP.objects.all().select_related('MGR').filter(MGR__job='sales')
+    QLEMO=EMP.objects.all().select_related('MGR').filter(MGR__hiredate__year__gt='2024')
+    QLEMO=EMP.objects.all().select_related('MGR').filter(job='accounting')
+    QLEMO=EMP.objects.all().select_related('MGR').filter(job='accounting')
+    QLEMO=EMP.objects.all().select_related('MGR')
+    QLEMO=EMP.objects.all().select_related('MGR').filter(MGR__ename='hardhik')
+    	
+
+
+
+
+    d1={'QLEMO':QLEMO}
+    return render(request,'EmpToMgrJoin.html',d1) 
+
+
+
+
+def EmpToDEPTMGRJoin(request):
+    QLEDMO=EMP.objects.all().select_related('dept_no','MGR')
+    QLEDMO=EMP.objects.all().select_related('dept_no','MGR').filter(ename='virat')
+    QLEDMO=EMP.objects.all()
+    d={'QLEDMO':QLEDMO}
+    return render(request,'EmpToDEPTMGRJoin.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def insert_DEPT(request):
+    dno=int(input('enter dept number'))
+    dn=input('enter dname')
+    l=input('enter dloc')
+    DTO=DEPT.objects.get_or_create(dept_no=dno,dname=dn,dloc=l)
+    if DTO[1]:
+        return HttpResponse(f'{dno} is created')
+    else:
+        return HttpResponse(f'{dno} is already exists')
+
+def insert_EMP(request):
+    eno = int(input('enter empno'))
+    en = input('enter ename')
+    job = input('enter job')
+    sal = float(input('enter the sal'))
+    hd = input('enter date')
+    comm = input('enter commission')
+    
+    if comm:
+        comm = float(comm)
+    else:
+        comm = None
+    
+    dept_no = int(input('enter deptno'))
+    DO = DEPT.objects.get(dept_no=dept_no)  
+
+    MGR = input('enter mgr')
+    MO = EMP.objects.filter(empno=int(MGR)).first() if MGR else None  
+
+    TEO = EMP.objects.get_or_create(
+        empno=eno, ename=en, job=job, sal=sal, comm=comm, dept_no=DO, MGR=MO, hiredate=hd
+    )
+
+    if TEO[1]:
+        return HttpResponse(f'{eno} is created')
+    else:
+        return HttpResponse(f'{eno} already exists')
